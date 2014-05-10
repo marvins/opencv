2f243fcf1d9e88bef0e74c823172f86284ce6bfd
Marvin Smith <marvin_smith1@me.com>
2014-05-10 10:48:24 -0700

    Adding the base files for the GDAL Module.  I need to figure out how to add the GDAL Project to CMake.

---
+++ samples/cpp/gdal_image.cpp
@@ -0,0 +1,18 @@
+/**
+ * gdal_image.cpp -- Load GIS data into OpenCV Containers using the Geospatial Data Abstraction Library
+*/
+
+/// OpenCV Headers
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+
+/**
+ * Main Function
+*/
+int main( int argc, char* argv[] ){
+
+   ///
+
+}
